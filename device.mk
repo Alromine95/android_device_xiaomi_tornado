b7ac09c83ce50bead2c841164d03d34556b7fe2a
Alromine95 <abhinavsingh.me5@gmail.com>
2026-07-26 17:51:45 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6835
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6835 \
     libgptutils \
     libz \
